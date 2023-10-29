7891a32aea330df943153699ef5b0c39b0aa663c
@eleven <97612446+RabiCB@users.noreply.github.com>
2023-10-30 00:01:08 +0545

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const imageDownloader = require("image-downloader");
 const multer = require("multer");
 const fs = require("fs");
 app.use("/uploads", express.static(__dirname + "/uploads"));
-const allowedOrigin = 'https://airbnbclone-3off.onrender.com';
+const allowedOrigin = 'https://getit-two.vercel.app/';
 app.use(
   cors({
     origin: allowedOrigin,
@@ -29,7 +29,7 @@ app.use(
 app.use(CookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://airbnbclone-3off.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://getit-two.vercel.app/');
  
 
   next();
